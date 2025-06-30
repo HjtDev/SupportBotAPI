abb6eddea6b1920512453e9ac1855bacfb8d979c
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-06-30 14:26:23 +0330

    feat(database, admin): add database configuration and admin panel settings
    Configured the Django database settings and enabled the admin panel
    for managing models and data through the Django admin interface.
    
    This setup includes necessary migrations and admin registration
    to facilitate backend management in the Django REST project.

--- SupportBotAPI/settings.py
+++ SupportBotAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'SupportBotAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supportbotapi',
+        'USER': 'supportbotapi',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
